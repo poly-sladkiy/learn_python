98150ffccf3ee7d7351205f865fba578578d2855
Konstantin <ignakov37@gmail.com>
2020-08-17 21:36:48 +0300

    Добавил для request вывод в bytes, str, json для обычной аудентефикации

--- 2_network.py
+++ 2_network.py
@@ -1,5 +1,10 @@
 import requests
 
-response = requests.get('https://httpbin.org/basic-auth/user/passwd', auth='user', 'passwd')
+# базовая аутификация
+response = requests.get('https://httpbin.org/basic-auth/user/passwd', auth=('user', 'passwd'))
 print(f'Тело ответа {response.content}')
 print(f'Код состояния {response.status_code}')
+
+print(f'Тело ответа в bytes {response.content}')
+print(f'Тело ответа в str {response.text}')
+print(f'Тело ответа в json {response.json()}')
