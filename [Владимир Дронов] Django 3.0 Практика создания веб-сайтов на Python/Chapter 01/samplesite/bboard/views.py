066fa8e01b5d0e8438a17aa0fc2629fa23ec67c9
Konstantin Ignakov <ignakov37@gmail.com>
2021-06-02 22:32:33 +0300

    Отладочный веб-сервер, контролеры, маршруты и маршрутизатор, модели, миграции, консоль django, моделишаблоны

---
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb
+
+
+def index(requests):
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+
+    return HttpResponse(template.render(context, requests))
