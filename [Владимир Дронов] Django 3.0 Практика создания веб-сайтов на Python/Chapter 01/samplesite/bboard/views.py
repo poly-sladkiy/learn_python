27bacf19cb464964efe8e7ab063ab7b703863fcc
Konstantin Ignakov <ignakov37@gmail.com>
2021-06-03 10:52:44 +0300

    URL-параметры и параметризованные вопросы, обратное разрешение интернет адресов

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/views.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 
-from .models import Bb
+from .models import Bb, Rubric
 
 
 def index(requests):
     bbs = Bb.objects.all()
-    return render(requests, 'bboard/index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(requests, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    currunt_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {
+        'bbs': bbs,
+        'rubrics': rubrics,
+        'current_rubrics': currunt_rubric,
+    }
+
+    return render(request, 'bboard/by_rubric.html', context)
