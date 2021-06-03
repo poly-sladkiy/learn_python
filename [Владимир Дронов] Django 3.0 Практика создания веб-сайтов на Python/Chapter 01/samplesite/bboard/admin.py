d2a6b308ca7ffdff282b67a560fb549f74c1e76d
Konstantin Ignakov <ignakov37@gmail.com>
2021-06-03 09:49:44 +0300

    Строковое представление модели

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/admin.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/admin.py
@@ -5,7 +5,7 @@ from .models import Rubric
 
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content',)
 
--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/models.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/models.py
@@ -19,6 +19,9 @@ class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True,
                             verbose_name='Название')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name_plural = 'Рубрики'
         verbose_name = 'Рубрика'
