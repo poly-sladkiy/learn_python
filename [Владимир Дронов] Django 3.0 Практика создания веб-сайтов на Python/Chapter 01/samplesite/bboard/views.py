754e9ab9044f45d05e11d8728227503197c22ee9
Konstantin Ignakov <ignakov37@gmail.com>
2021-06-03 09:04:28 +0300

    Django.shortcuts, косметические изменения

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/views.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/views.py
@@ -1,12 +1,8 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Bb
 
 
 def index(requests):
-    template = loader.get_template('bboard/index.html')
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-
-    return HttpResponse(template.render(context, requests))
+    return render(requests, 'bboard/index.html', {'bbs': bbs})
