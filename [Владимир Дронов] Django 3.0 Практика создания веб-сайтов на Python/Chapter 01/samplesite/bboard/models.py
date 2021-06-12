9d4a8e2423fee32a011aaa41417f6617e2d38191
Konstantin Ignakov <ignakov37@gmail.com>
2021-06-12 16:56:00 +0300

    Создание полей со списком, параметры самой модели,

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/models.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Measure(models.Model):
+    class Measurements(float, models.Choices):
+        METERS = 1.0, 'Метры'
+        FEET = 0.3048, 'Фунты'
+        YARDS = 0.9144, 'Ярды'
+
+    measurement = models.FloatField(choices=Measurements.choices)
+
+
 class AdvUser(models.Model):
     is_activated = models.BooleanField(default=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -17,17 +26,48 @@ class Machine(models.Model):
 
 
 class Bb(models.Model):
+    # Создание полей со списком
+    class Kinds(models.TextChoices):
+        """
+        Способен хранить значение из ограниченного набора, заданного в особом перечне
+        """
+        BUY = 'b', 'Куплю'
+        SELL = 's', 'Продам'
+        EXCHANGE = 'c', 'Обменяю'
+        RENT = 'r'
+
+        __empty__ = 'Выберете тип публикуемого объявления'
+
+    kind = models.CharField(max_length=1, choices=Kinds.choices, default=Kinds.SELL)
+
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+
     rubric = models.ForeignKey('Rubric', null=True,
                                on_delete=models.PROTECT, verbose_name='Рубрика')
 
     class Meta:
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявление'
-        ordering = ['-published']
+
+        ''' Cannot be together '''
+        # ordering = ['-published']
+        order_with_respect_to = 'rubric'
+
+        unique_together = (
+            ('title', 'published'),
+            ('title', 'price', 'rubric'),
+        )
+
+        get_latest_by = '-published'
+
+        indexes = [
+            models.Index(fields=['-published', 'title'],
+                         name='bb_partial',
+                         condition=models.Q(price__lte=10000))
+        ]
 
 
 class Rubric(models.Model):
