1df19f5ac1cf33597ff75afe17ea3465f1217aac
Konstantin Ignakov <ignakov37@gmail.com>
2021-06-30 12:09:23 +0300

    Указание шаблонных путей в виде регулярных выражений.

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/urls.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import index, by_rubric, BbCreateView
 
 urlpatterns = [
-    path('add/', BbCreateView.as_view(), name='add'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    # Использование re для совместимости с django 1.x
+    re_path(r'^add/$', BbCreateView.as_view(), name='add'),
+    re_path(r'^(?P<rubric_id>[0-9]*)/$', by_rubric, name='by_rubric'),
+    re_path(r'^$', index, name='index'),
+
+    # path('add/', BbCreateView.as_view(), name='add'),
+    # path('<int:rubric_id>/', by_rubric, name='by_rubric'),
 
     # Корневой маршрут, указывающий на "корень" приложения bboard
-    path('', index, name='index'),
+    # path('', index, name='index'),
 ]
