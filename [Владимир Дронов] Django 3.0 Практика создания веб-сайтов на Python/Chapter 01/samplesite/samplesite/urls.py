fb434d77dc7b5ae994c8a7c12d6e9ab5594e0e69
Konstantin Ignakov <ignakov37@gmail.com>
2021-07-17 13:06:33 +0300

    Убрал ненужный url.

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/samplesite/urls.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/samplesite/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('bboard.urls')),
-    path('bboard/', include('bboard.urls')),
 
     path('admin/', admin.site.urls),
 ]
