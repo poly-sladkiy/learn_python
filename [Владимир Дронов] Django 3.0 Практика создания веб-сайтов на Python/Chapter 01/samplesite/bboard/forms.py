32aead5390f8fd1644434fc0bc0a233494500ad3
Konstantin Ignakov <ignakov37@gmail.com>
2021-07-19 17:40:36 +0300

    Создние форм путём быстрого объявления. Использоание класса ModelForm.

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/forms.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/forms.py
@@ -1,14 +1,14 @@
-from django.forms import ModelForm, modelform_factory, DecimalField
+from django.forms import ModelForm, DecimalField
 from django.forms.widgets import Select
 
 from .models import Bb
 
 
-BbForm = modelform_factory(
-    Bb,
-    fields=('title', 'content', 'price', 'rubric'),
-    labels={'title': 'Название товара'},
-    help_texts={'rubric': 'Не забудьте выбрать рубрику!'},
-    field_classes={'price': DecimalField},
-    widgets={'rubric': Select(attrs={'size': 8})}
-)
+class BbForm(ModelForm):
+    class Meta:
+        model = Bb
+        fields = ('title', 'content', 'price', 'rubric')
+        label = {'title': 'Название товара'},
+        help_texts = {'rubric': 'Не забудьте выбрать рубрику!'}
+        field_classes = {'price': DecimalField}
+        widgets = {'rubric': Select(attrs={'size': 2})}
