fe068c706f99c0a1635e42b1487fea51d116e22d
Konstantin Ignakov <ignakov37@gmail.com>
2021-07-19 17:30:21 +0300

    Формы связанные с моделями. Создание форм с помощью фабрики классов. Использование modelform_factory.

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/forms.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/forms.py
@@ -1,9 +1,14 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, modelform_factory, DecimalField
+from django.forms.widgets import Select
 
 from .models import Bb
 
 
-class BbForm(ModelForm):
-    class Meta:
-        model = Bb
-        fields = ('title', 'content', 'price', 'rubric')
+BbForm = modelform_factory(
+    Bb,
+    fields=('title', 'content', 'price', 'rubric'),
+    labels={'title': 'Название товара'},
+    help_texts={'rubric': 'Не забудьте выбрать рубрику!'},
+    field_classes={'price': DecimalField},
+    widgets={'rubric': Select(attrs={'size': 8})}
+)
