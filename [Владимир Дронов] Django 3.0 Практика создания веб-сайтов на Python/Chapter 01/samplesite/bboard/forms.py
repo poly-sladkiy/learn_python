96059ad3af74ebbd41ff3487cdb7c3d6b483797c
Konstantin Ignakov <ignakov37@gmail.com>
2021-07-21 17:17:55 +0300

    Валидация в формах. Валидация путем переопределения методов формы. Валидация формы.

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/forms.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/forms.py
@@ -1,10 +1,14 @@
 from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
 
 from .models import Bb, Rubric
 
 
 class BbForm(forms.ModelForm):
-    title = forms.CharField(label='Название товара')
+    title = forms.CharField(label='Название товара',
+                            validators=[validators.RegexValidator(r'^.{4,}$')],
+                            error_messages={'invalid': 'Слишком короткое название товара.'})
     content = forms.CharField(label='Описание',
                               widget=forms.widgets.Textarea())
     price = forms.DecimalField(label='Цена', decimal_places=2)
@@ -17,6 +21,26 @@ class BbForm(forms.ModelForm):
     в котором можно указать значение size, если его не передавать,
     то вывод будет как обычной качелькой
     '''
+
+    def clean_title(self):
+        val = self.cleaned_data['title']
+        if val == 'Прошлогодний снег':
+            raise ValidationError('К продаже не допускается')
+        return val
+
+    def clean(self):
+        super().clean()
+        errors = {}
+
+        if not self.cleaned_data['content']:
+            errors['content'] = ValidationError(
+                'Укажите описание продаваемого товара!')
+        if self.cleaned_data['price'] < 0:
+            errors['price'] = ValidationError(
+                'Укажите неотрицательное значение цены!')
+
+        if errors:
+            raise ValidationError(errors)
 
     class Meta:
         model = Bb
