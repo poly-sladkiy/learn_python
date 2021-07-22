baf717b988ab6eb86681791afde622e7b2fb8973
Konstantin Ignakov <ignakov37@gmail.com>
2021-07-22 11:19:54 +0300

    Реализация переупорядочивания записей.

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/forms.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core import validators
 from django.core.exceptions import ValidationError
 from django.forms import modelformset_factory
+from django.forms.formsets import ORDERING_FIELD_NAME
 from django.shortcuts import redirect, render
 
 from .models import Bb, Rubric
@@ -9,12 +10,18 @@ from .models import Bb, Rubric
 
 def rubric(request):
     RubricFormSet = modelformset_factory(Rubric, fields=('name',),
-                                         can_delete=True)
+                                         can_order=True, can_delete=True)
     if request.method == 'POST':
         formset = RubricFormSet(request.POST)
         if formset.is_valid():
-            formset.save()
+            for form in formset:
+                if form.cleaned_data:
+                    rubric = form.save(commit=False)
+                    rubric.order = form.cleaned_data[ORDERING_FIELD_NAME]
+                    rubric.save()
+
             return redirect('bboard:index')
+
     else:
         formset = RubricFormSet()
     context = {'formset': formset}
