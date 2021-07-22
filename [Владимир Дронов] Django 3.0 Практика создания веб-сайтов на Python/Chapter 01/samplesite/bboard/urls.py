344fa84093295e445e4362a2e3fe0e99f92dd41d
Konstantin Ignakov <ignakov37@gmail.com>
2021-07-22 13:35:38 +0300

    Аутентификация и служебные процедуры. LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView. Вернул убравший код - проверять миграции, ошибки могут в их не выполнении или отсутствии по причине случайного удаления.

--- [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/urls.py
+++ [Владимир Дронов] Django 3.0 Практика создания веб-сайтов на Python/Chapter 01/samplesite/bboard/urls.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, \
+    PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path
 
-from .views import index, BbCreateView, BbByRubricView, BbDetailView
+from .views import index, BbCreateView, BbByRubricView, BbDetailView, rubrics
 
 # Имя приложения
 app_name = 'bboard'
@@ -9,6 +11,32 @@ urlpatterns = [
     path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
     path('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
+    path('rubrics/', rubrics, name='edit_rubrics'),
+
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+
+    path('accounts/password_change/', PasswordChangeView.as_view(
+        template_name='registration/change_password.html'),
+        name='change_password'),
+    path('accounts/password_change/done', PasswordChangeDoneView.as_view(
+        template_name='registration/password_changed.html'), name='password_change_done'),
+    path('accounts/password_reset/', PasswordResetView.as_view(
+        template_name='registration/reset_password.html',
+        subject_template_name='registration/reset_subject.txt',
+        email_template_name='registration/reset_email.txt'),
+        name='password_reset'),
+    path('accounts/password_reset/done/', PasswordResetDoneView.as_view(
+        template_name='registration/email_sent.html'),
+        name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>',
+         PasswordResetConfirmView.as_view(
+             template_name='registration/confirm_password.html'),
+         name='password_reset_confirm'),
+    path('accounts/reset/done/',
+         PasswordResetCompleteView.as_view(
+             template_name='registration/password_confirmed.html'),
+         name='password_reset_complete'),
 
     # Корневой маршрут, указывающий на "корень" приложения bboard
     path('', index, name='index'),
