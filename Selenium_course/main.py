4d2e4064d5fcb8ccb7353ccdf3ab3a34953dabfd
Konstantin Ignakov <ignakov37@gmail.com>
2021-09-03 20:13:08 +0300

    Set proxy.

--- Selenium_course/main.py
+++ Selenium_course/main.py
@@ -5,6 +5,7 @@ url = 'https://www.instagram.com'
 
 options = webdriver.ChromeOptions()
 options.add_argument("user-agent=Mozilla/5.0 (Windows10; Intel Mac OS X 10_15_7)")
+options.add_argument("--proxy-server=PROXY")
 
 driver = webdriver.Chrome(
     executable_path='./chromedriver',
@@ -12,7 +13,7 @@ driver = webdriver.Chrome(
 )
 
 try:
-    driver.get(url)
+    driver.get("https://2ip.ru")
     sleep(5)
 except Exception as ex:
     print(ex)
