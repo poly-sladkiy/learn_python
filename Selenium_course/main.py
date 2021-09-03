354efd08b692edf76c308b81ce7b7d10ade043d3
Konstantin Ignakov <ignakov37@gmail.com>
2021-09-03 20:01:37 +0300

    Manual task of the user agent.

--- Selenium_course/main.py
+++ Selenium_course/main.py
@@ -2,7 +2,14 @@ from selenium import webdriver
 from time import sleep
 
 url = 'https://www.instagram.com'
-driver = webdriver.Chrome(executable_path='./chromedriver')
+
+options = webdriver.ChromeOptions()
+options.add_argument("user-agent=Mozilla/5.0 (Windows10; Intel Mac OS X 10_15_7)")
+
+driver = webdriver.Chrome(
+    executable_path='./chromedriver',
+    options=options,
+)
 
 try:
     driver.get(url)
