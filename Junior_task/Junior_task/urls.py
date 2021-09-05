736b990e6e5938727e46429c2bc71e7b4d798f5c
Konstantin Ignakov <ignakov37@gmail.com>
2021-09-05 14:55:35 +0300

    Create template file for html files.

--- Junior_task/Junior_task/urls.py
+++ Junior_task/Junior_task/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from Junior_task.views import home
+from Junior_task.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home)
+    path('home/', home),
+    path('about/', about)
 ]
--- Junior_task/Junior_task/views.py
+++ Junior_task/Junior_task/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     context = {'name': 'Django'}
     return render(request, 'home.html', context=context)
+
+
+def about(request):
+    return render(request, 'about.html', )
