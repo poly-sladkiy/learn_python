ab40485a21db09f9556f1385f13f916a923bd32d
Konstantin Ignakov <ignakov37@gmail.com>
2021-09-07 18:19:13 +0300

    Create detail view for a city.

--- Junior_task/cities/views.py
+++ Junior_task/cities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from cities.models import City
 
@@ -7,8 +7,17 @@ __all__ = (
 )
 
 
-def home(request):
-    qs = City.objects.all()
-    context = {'cities': qs}
+def home(request, pk=None):
+    if pk:
+        # city = City.objects.filter(pk=pk).first()
+        # city = City.objects.get(pk=pk)
+        city = get_object_or_404(City, pk=pk)
 
-    return render(request, 'cities/home.html', context=context)
+        context = {'city': city}
+        return render(request, 'cities/detail.html', context)
+
+    else:
+        qs = City.objects.all()
+        context = {'cities': qs}
+
+        return render(request, 'cities/home.html', context=context)
