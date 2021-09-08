e5db7295df3ff5f643dff952eb87cde872f59b26
Konstantin Ignakov <ignakov37@gmail.com>
2021-09-08 13:13:47 +0300

    Create DetailView for City.

--- Junior_task/cities/views.py
+++ Junior_task/cities/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
 from cities.models import City
 
 __all__ = (
     'home',
+    'CityDetailView'
 )
 
 
 def home(request, pk=None):
-    if pk:
-        # city = City.objects.filter(pk=pk).first()
-        # city = City.objects.get(pk=pk)
-        city = get_object_or_404(City, pk=pk)
+    # if pk:
+    # city = City.objects.filter(pk=pk).first()
+    # city = City.objects.get(pk=pk)
+    # city = get_object_or_404(City, pk=pk)
+    #
+    # context = {'city': city}
+    # return render(request, 'cities/detail.html', context)
 
-        context = {'city': city}
-        return render(request, 'cities/detail.html', context)
+    qs = City.objects.all()
+    context = {'cities': qs}
 
-    else:
-        qs = City.objects.all()
-        context = {'cities': qs}
+    return render(request, 'cities/home.html', context=context)
 
-        return render(request, 'cities/home.html', context=context)
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
