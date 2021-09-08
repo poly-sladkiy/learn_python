75ef2bef707ba7252042d564a5d6673e6dfbe444
Konstantin Ignakov <ignakov37@gmail.com>
2021-09-08 14:05:39 +0300

    Modify CityForm with Bootstrap and edit in base.html.

--- Junior_task/cities/forms.py
+++ Junior_task/cities/forms.py
@@ -9,6 +9,14 @@ class HtmlForm(forms.Form):
 
 class CityForm(forms.ModelForm):
 
+    name = forms.CharField(
+        label='Город',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите название города',
+        })
+    )
+
     class Meta:
         model = City
         fields = ('name',)
