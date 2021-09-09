6794d6fbef25b3d4a1bb76ff22d901410a1ef5c3
Konstantin Ignakov <ignakov37@gmail.com>
2021-09-09 20:58:16 +0300

    Create model trains.

--- Junior_task/trains/models.py
+++ Junior_task/trains/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from cities.models import City
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Номер поезда')
+    travel_time = models.PositiveSmallIntegerField(verbose_name='Время в пути')
+
+    from_city = models.ForeignKey(City,
+                                  on_delete=models.CASCADE,
+                                  related_name='from_city_set',
+                                  verbose_name='Город отправления')
+
+    to_city = models.ForeignKey('cities.City',
+                                on_delete=models.CASCADE,
+                                related_name='to_city_set',
+                                verbose_name='Город прибытия')
+
+    def __str__(self):
+        return f'Поезд № {self.name} из {self.from_city} в {self.to_city} время отправления {self.travel_time}'
+
+    class Meta:
+        verbose_name = 'Поезд'
+        verbose_name_plural = 'Поезда'
+        ordering = ['name',]
