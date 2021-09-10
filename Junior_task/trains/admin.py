4abeb724bda3dd9443eb4ece01baaeda0482e9e0
Konstantin Ignakov <ignakov37@gmail.com>
2021-09-10 08:00:28 +0300

    Edit model view in admin site.

--- Junior_task/trains/admin.py
+++ Junior_task/trains/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from trains.models import Train
 
-admin.site.register(Train)
+
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ('name', 'from_city', 'to_city', 'travel_time',)
+    list_editable = ('travel_time',)
+
+    class Meta:
+        model = Train
+
+
+admin.site.register(Train, TrainAdmin)
--- Junior_task/trains/apps.py
+++ Junior_task/trains/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class TrainsConfig(AppConfig):
     name = 'trains'
+    verbose_name = 'Поезда'
