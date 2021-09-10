c22642f30d57885686b3f126a857a9ee792e6061
Konstantin Ignakov <ignakov37@gmail.com>
2021-09-10 18:07:18 +0300

    Add list editable.

--- Junior_task/trains/admin.py
+++ Junior_task/trains/admin.py
@@ -5,6 +5,7 @@ from trains.models import Train
 
 class TrainAdmin(admin.ModelAdmin):
     list_display = ('name', 'from_city', 'to_city', 'travel_time')
+    list_editable = ('travel_time',)
 
     class Meta:
         model = Train
