954d593ba98a534751446483aa1f7fce3b963b9e
Konstantin Ignakov <ignakov37@gmail.com>
2021-10-28 16:42:12 +0300

    In the future, you will save the route.

--- Junior_task/Junior_task/urls.py
+++ Junior_task/Junior_task/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from routes.views import home, find_routes
+from routes.views import home, find_routes, add_route
 
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
-    path('find_routes/', find_routes, name='find_routes')
+    path('find_routes/', find_routes, name='find_routes'),
+    path('add_route/', add_route, name='add_route'),
 ]
