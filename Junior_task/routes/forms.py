6070902bcb81cc3fec0e94578d057d868e557529
Konstantin Ignakov <ignakov37@gmail.com>
2021-10-28 16:53:57 +0300

    Create form for routes.

--- Junior_task/routes/forms.py
+++ Junior_task/routes/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 
 from cities.models import City
+from routes.models import Route
+from trains.models import Train
 
 
 class RouteForm(forms.Form):
-
     from_city = forms.ModelChoiceField(
         label='Готод отправления',
         queryset=City.objects.all(),
@@ -39,3 +40,44 @@ class RouteForm(forms.Form):
             'placeholder': 'Время в пути',
         })
     )
+
+
+class RouteModelForm(forms.ModelForm):
+    name = forms.CharField(
+        label='Название маршрута',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите название маршрута'
+            }
+        )
+    )
+
+    from_city = forms.ModelChoiceField(
+        queryset=City.objects.all(),
+        widget=forms.HiddenInput(),
+    )
+
+    to_city = forms.ModelChoiceField(
+        queryset=City.objects.all(),
+        widget=forms.HiddenInput(),
+    )
+
+    trains = forms.ModelMultipleChoiceField(
+        queryset=Train.objects.all(),
+        required=False,
+        widget=forms.SelectMultiple(
+            attrs={
+                'class': 'form-control d-none',
+            }
+        )
+    )
+
+    travel_times = forms.IntegerField(
+        label='Время в пути',
+        widget=forms.HiddenInput(),
+    )
+
+    class Meta:
+        model = Route
+        fields = '__all__'
