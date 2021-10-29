1eb84667388b27b8fef70346b7ce0953c1d5354e
Konstantin Ignakov <ignakov37@gmail.com>
2021-10-29 20:42:04 +0300

    Create login and logout functions.

--- Junior_task/Junior_task/urls.py
+++ Junior_task/Junior_task/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
+    path('accounts/', include(('accounts.urls', 'accounts'))),
     path('find_routes/', find_routes, name='find_routes'),
     path('add_route/', add_route, name='add_route'),
     path('save_route/', save_route, name='save_route'),
